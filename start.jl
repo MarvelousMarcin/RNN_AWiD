36ee6c041f173d1e0fb426daa76bc95f139b1399
barbaraborkowska <borkowska.barbara510@gmail.com>
2024-06-23 18:50:48 +0200

    notebook to .jl

---
+++ start.jl
@@ -0,0 +1,19 @@
+include("./src/network/rnn.jl")
+
+using MLDatasets: MNIST
+using Flux
+
+train_data = MNIST(:train)
+test_data = MNIST(:test)
+
+x_train = reshape(train_data.features, 28 * 28, :)
+y_train = Flux.onehotbatch(train_data.targets, 0:9)
+x_test = reshape(test_data.features, 28 * 28, :)
+y_test = Flux.onehotbatch(test_data.targets, 0:9)
+
+include("./src/network/rnn_custom.jl")
+r = RNN_CUST(5, 100, 15e-3, [[rnn, tanh], [dense, identity]])
+
+@profview train(r, x_train, y_train)
+
+test(r,x_test, y_test)
