d31f657062010d5abef8baff346a241def4e1534
barbaraborkowska <borkowska.barbara510@gmail.com>
2024-06-23 22:33:53 +0200

    rnn backward cleanup

--- src/network/overloading.jl
+++ src/network/overloading.jl
@@ -22,49 +22,37 @@ function forward(::BroadcastedOperator{typeof(rnn)}, x, w, w_rec, b, h_prev)
 end
 
 mutable struct Gradients
-    grad_x::AbstractArray{Float64}
-    grad_w::AbstractArray{Float64}
-    grad_w_rec::AbstractArray{Float64}
-    grad_b::AbstractArray{Float64}
-    grad_h_prev::AbstractArray{Float64}
+    grad_h::Vector{Float64}
+    grad_x::Vector{Float64}
+    grad_w::Matrix{Float64}
+    grad_w_rec::Matrix{Float64}
+    grad_b::Vector{Float64}
+    grad_h_prev::Vector{Float64}
 end
 
-function init_gradients(x, w, w_rec)
-    h_prev = zeros(64)
-
-    grad_h = zeros(Float32, 64)
-    grad_x = zeros(Float32, size(w.output, 2), size(grad_h, 2))
-    grad_w = zeros(Float32, size(h_prev, 1), size(x.output, 1))
-    grad_w_rec = zeros(Float64, size(h_prev, 1), size(h_prev, 1))
-
-    grad_b = zeros(Float64, size(h_prev, 1), 1)
-    grad_h_prev = zeros(Float64, size(w_rec.output, 2), size(grad_h, 2))
+function init_gradients(input_size, output_size)
+    grad_h = zeros(output_size)
+    grad_x = zeros(input_size)
+    grad_w = zeros(output_size, input_size)
+    grad_w_rec = zeros(output_size, output_size)
+    grad_b = zeros(output_size)
+    grad_h_prev = zeros(output_size)
     
-    return Gradients(grad_x, grad_w, grad_w_rec, grad_b, grad_h_prev)
-end
-
-function reset_gradients!(grads::Gradients)
-    grads.grad_x .= 0
-    grads.grad_w .= 0
-    grads.grad_w_rec .= 0
-    grads.grad_b .= 0
-    grads.grad_h_prev .= 0
+    return Gradients(grad_h, grad_x, grad_w, grad_w_rec, grad_b, grad_h_prev)
 end
 
 # Optimized backward function
 import LinearAlgebra: mul!
-
 backward(op::BroadcastedOperator{typeof(rnn)}, x, w, w_rec, b, h_prev, g) = let
     h = forward(op, x, w, w_rec, b, h_prev)  # OPTIMIZATION
     global grads
-    grad_h = g .* (1 .- h.^2)
+    grads.grad_h = g .* (1 .- h.^2)
 
-    mul!(grads.grad_x, w', grad_h)
-    mul!(grads.grad_w, grad_h, x')
-
-    grads.grad_w_rec = grad_h * h_prev'
-    grads.grad_b = sum(grad_h, dims=2)
-    grads.grad_h_prev = w_rec' * grad_h
+    mul!(grads.grad_x, w', grads.grad_h)
+    mul!(grads.grad_w, grads.grad_h, x')
+    mul!(grads.grad_h_prev, w_rec', grads.grad_h)
+    grads.grad_w_rec = grads.grad_h * h_prev'
+    grads.grad_b = sum(grads.grad_h, dims=2)
  
     return tuple(grads.grad_x, grads.grad_w, grads.grad_w_rec, grads.grad_b, grads.grad_h_prev)
 end
